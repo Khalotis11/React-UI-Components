21fd347e770286759e3cc0e7b3af32c57a186a02
Khalotis11 <moekhaloti@gmail.com>
2019-05-29 23:19:05 +0000

    Added Header Components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from "react";
 import "./Header.css";
+
+import { ImageThumbnail } from "./ImageThumbnail";
+import { HeaderTitle } from "./HeaderTitle";
+import { HeaderContent } from "./HeaderContent";
+
+export const HeaderContainer = () => {
+  return (
+    <div className="header-container">
+      <ImageThumbnail />
+      <div className="title-content">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from "react";
 import "./Header.css";
+
+export const HeaderTitle = () => {
+  return (
+    <div className="header-title">
+      <h2>Lambda School </h2>
+      <p>
+        <span>@LambdaSchool • 26 Jan</span>
+      </p>
+    </div>
+  );
+};
