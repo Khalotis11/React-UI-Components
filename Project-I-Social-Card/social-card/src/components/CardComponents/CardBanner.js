6d5b610ae372349add25023b4224bd1686d42ca0
Khalotis11 <moekhaloti@gmail.com>
2019-05-30 00:07:24 +0000

    Added Card Components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
 import React from "react";
 import "./Card.css";
+
+export const CardBanner = () => {
+  return (
+    <div>
+      <img
+        className="card-banner"
+        src=" https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+        alt="main image"
+      />
+    </div>
+  );
+};
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from "react";
 import "./Card.css";
+
+export const CardContent = () => {
+  return (
+    <div className="card-content">
+      <p>
+        React makes it painless to create interactive Uis. Design simple views
+        for each state in your application
+      </p>
+      <a href="https://reactjs.org/" className="card-content-site">
+        reactjs.org
+      </a>
+    </div>
+  );
+};
